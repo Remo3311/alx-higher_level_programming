29a0d4306a6cbe7cf5566ff57f5157c0b3b14c94
Remo3311 <remo33553311@gmail.com>
2024-01-09 09:56:29 -0800

    0x0B-python-input_output

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
-"""6-load_from_json_file.py"""
+
+
 import json
 
 
-def load_from_json_file(filename):
-    """Creates an Object from a “JSON file”
-    Args:
-        filename: name of JSON file to decode to object
-    Returns: object decoded from filename
-    """
-    with open(filename, "r", encoding="utf-8") as f:
-        obj = json.load(f)
-
-    return obj
+def save_to_json_file(my_obj, filename):
+    with open(filename, "w", encoding="UTF-8") as f:
+        json.dump(my_obj, f)
